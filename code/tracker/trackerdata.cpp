2faeeeb00025ded09d4c7dfc9b988efc799988ff
Vivek Majithia <48577858+vivekm07@users.noreply.github.com>
2021-04-18 23:03:15 +0530

    Add files via upload

---
+++ code/tracker/trackerdata.cpp
@@ -0,0 +1,30 @@
+/************************************************************************
+** Created by: Vivek Majithia *******************************************
+************************************************************************/
+
+//**********************************************************************
+//class to maintain Tracker Data
+//**********************************************************************
+#include "trackerheader.h"
+
+class trackerdata
+{
+  public:
+    string shash;
+    string csocket;
+    string cfpath;
+
+    trackerdata()
+    {
+        shash = "";
+        csocket = "";
+        cfpath = "";
+    }
+
+    trackerdata(string hash, string ipport, string path)
+    {
+        shash = hash;
+        csocket = ipport;
+        cfpath = path;
+    }
+};
---
+++ code/tracker/trackerheader.h
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <openssl/sha.h>
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <pthread.h>
+
+using namespace std;
+
+void writelog(string str);
